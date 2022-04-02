9329f017074b0563a1a2a7e6ecf8464ca63d6b34
Pitrick <89285654+Pitrick3141@users.noreply.github.com>
2022-04-02 07:48:27 -0400

    4.1 update

--- scene1.py
+++ scene1.py
@@ -1,10 +1,19 @@
 backgroundPicture = 'scene1.jpg'
-lane = 107
+lane = 127
 lane_start = 15
-rows = [0,80,111,141,172,202,232,264,295,326,357,388,419]
-columns = [0,36,60,85,136,160,185]
+maxRow = 32
+maxColumn = 6
+prop = 30
+rows = [0,41,73,101,132,161,191,220,250,280,312,340,369,398,426,466,499,533,562,589,618,652,679,708,737,768,799,830,859,889,917,950,976]
+columns = [0,50,76,100,150,172,196]
 seatsUsed = []
 rowBusy = []
-for i in range(13):
-    seatsUsed.append([False,False,False,False,False,False,False])
+
+
+def initialize():
+    for i in range(33):
+        seatsUsed.append([False,False,False,False,False,False,False])
+    seatsUsed[1][1] = seatsUsed[1][2] = seatsUsed[1][3] = True
+
+initialize()
 print("Scene 1 Loaded!")
--- simulation.py
+++ simulation.py
@@ -1,4 +1,7 @@
+from asyncio import threads
+from asyncio.windows_events import NULL
 from cProfile import label
+from glob import glob
 from textwrap import fill
 from tkinter import *
 from tokenize import String
@@ -21,45 +24,72 @@ def update_setting():
     passengerNumber = int(var_passenger_number.get())
 
     global maxSpeed
-    maxSpeed = int(var_max_speed.get())
+    maxSpeed = float(var_max_speed.get())
 
     global minSpeed
-    minSpeed = int(var_min_speed.get())
+    minSpeed = float(var_min_speed.get())
+    
+    global interval
+    interval = float(var_interval.get())
 
-    print("Settings Updated:\nPassenger Numbers {0}\nMax Speed {1}\nMin Speed {2}".format(passengerNumber,maxSpeed,minSpeed)
+    global playspeed
+    playspeed = float(var_playSpeed.get())
+    print("Settings Updated:\nPassenger Numbers {0}\nMax Speed {1}\nMin Speed {2}\nInterval {3} s\nPlay Speed {4}x".format(passengerNumber,maxSpeed,minSpeed,interval,playspeed)
     )
 def move_figure(passenger,figure,dx,dy):
     cv.move(figure,dx,dy)
     passenger.xpos += dx
     passenger.ypos += dy
 
-def genarate_passenger():
+def generate_passenger():
     global ID
     starttime = time.time()
-    speed = random.randint(minSpeed,maxSpeed)
-    rowspeed = random.random()*2+0.2
+    actual_speed = random.uniform(minSpeed,maxSpeed)
+    speed = actual_speed * currentScene.prop * 0.1 * playspeed
+    actual_rowspeed = random.uniform(minSpeed,maxSpeed) / 3
+    rowspeed = actual_rowspeed * currentScene.prop * 0.1 * playspeed
     strength = random.randint(1,5)
-    row = random.randint(1,12)
-    column = random.randint(1,6)
+    row = random.randint(1,currentScene.maxRow)
+    column = random.randint(1,currentScene.maxColumn)
     while(currentScene.seatsUsed[row][column]):
-        row = random.randint(1,12)
-        column = random.randint(1,6)
+        row = random.randint(1,currentScene.maxRow)
+        column = random.randint(1,currentScene.maxColumn)
     currentScene.seatsUsed[row][column] = True
     weight = [random.randint(1,100),random.randint(1,100)]
     randomized_passenger = People(ID,speed,rowspeed,strength,weight,(row,column),lane_start,lane)
     ID += 1
     passengers.append(randomized_passenger)
     endtime = time.time()
-    print("Generated random passenger: seat {0}, speed {1}, rowspeed {2}, bags{3}, using time {4}".format((row,column),speed,rowspeed,weight,endtime-starttime))
-    return randomized_passenger
+    print("[Generation] Generated random passenger: seat {}, speed {:.3f} m/s, rowspeed {:.3f} m/s, bags {}, using time {:.6f} s".format((row,column),actual_speed,actual_rowspeed,weight,endtime-starttime))
 
+def clear_simulation():
+    currentScene.seatsUsed.clear()
+    currentScene.initialize()
+    currentScene.rowBusy.clear()
+    passengers.clear()
+    cv.delete("all")
+    cv.create_image(0, 0, anchor='nw',image=image_file)
+    cv.create_circle(lane_start, lane, 10, fill='yellow')
+    cv.update()
+
+def replay():
+    currentScene.seatsUsed.clear()
+    currentScene.initialize()
+    currentScene.rowBusy.clear()
+    cv.delete("all")
+    cv.create_image(0, 0, anchor='nw',image=image_file)
+    cv.create_circle(lane_start, lane, 10, fill='yellow')
+    cv.update()
 #Settings
 passengerNumber = 10
-maxSpeed = 5
-minSpeed = 1
+maxSpeed = 2
+minSpeed = 0.8
+interval = 1
+playspeed = 1
 
 #current scene in use
 currentScene = scene1
+image_file = NULL
 
 #the y value of the starting point
 lane = currentScene.lane
@@ -76,6 +106,11 @@ ID = 0
 
 #is the simulation paused
 isPause = False
+
+#Time used
+generationTime = 0
+runTime = 0
+totalTime = 0
 
 class People:
     def __init__(self, ID,speed, rowspeed,strength,bags,seat,x,y):
@@ -101,7 +136,7 @@ class Behaviours(threading.Thread):
         self.threadID = threadID
         self.passenger = passenger
     def run(self):
-        print("[Behaviour Thread{0}] Thread started".format(self.threadID))
+        print("[Thread{0}] Thread started".format(self.threadID))
         starttime = time.time()
         #assign random color to the figure
         color = "#"+''.join([random.choice('ABCDEF0123456789') for i in range(6)])
@@ -111,34 +146,43 @@ class Behaviours(threading.Thread):
         currentScene.rowBusy.append((self.passenger.xpos,self.passenger.ID))
         while(self.passenger.xpos < currentScene.rows[self.passenger.seat_row]):
             #moving in walkthrough to one's row
+            currentSpeed = self.passenger.speed
+            if(self.passenger.xpos + self.passenger.speed > currentScene.rows[self.passenger.seat_row]):
+                currentSpeed = currentScene.rows[self.passenger.seat_row] - self.passenger.xpos
             next = 100000
             if(currentScene.rowBusy.index((self.passenger.xpos,self.passenger.ID)) < len(currentScene.rowBusy) - 1):
                 next = currentScene.rowBusy[currentScene.rowBusy.index((self.passenger.xpos,self.passenger.ID))+1][0]
             if(not isPause and next-self.passenger.xpos >= 10):
                 currentScene.rowBusy.remove((self.passenger.xpos,self.passenger.ID))
-                currentScene.rowBusy.append((self.passenger.xpos + self.passenger.speed,self.passenger.ID))
+                currentScene.rowBusy.append((self.passenger.xpos + currentSpeed,self.passenger.ID))
                 currentScene.rowBusy.sort(key=takeFirst)
-                move_figure(self.passenger,self.figure,self.passenger.speed,0)
+                move_figure(self.passenger,self.figure,currentSpeed,0)
                 #print("Current coordinate is ({0},{1})".format(self.passenger.xpos,self.passenger.ypos))
             time.sleep(0.1)
+        #time.sleep()
         currentScene.rowBusy.remove((self.passenger.xpos,self.passenger.ID))
+        currentSpeed = self.passenger.rowspeed
         if(self.passenger.seat_column >= 4):
             #moving down to one's seat
             while(self.passenger.ypos < currentScene.columns[self.passenger.seat_column]):
+                if(self.passenger.ypos + self.passenger.rowspeed > currentScene.columns[self.passenger.seat_column]):
+                    currentSpeed = currentScene.columns[self.passenger.seat_column] - self.passenger.ypos
                 if(not isPause):
-                    move_figure(self.passenger,self.figure,0,self.passenger.rowspeed)
+                    move_figure(self.passenger,self.figure,0,currentSpeed)
                     #print("Current coordinate is ({0},{1})".format(self.passenger.xpos,self.passenger.ypos))
                 time.sleep(0.1)
         else:
             #moving up to one's seat
             while(self.passenger.ypos > currentScene.columns[self.passenger.seat_column]):
+                if(self.passenger.ypos - self.passenger.rowspeed < currentScene.columns[self.passenger.seat_column]):
+                    currentSpeed = self.passenger.ypos - currentScene.columns[self.passenger.seat_column]
                 if(not isPause):
-                    move_figure(self.passenger,self.figure,0,-self.passenger.rowspeed)
+                    move_figure(self.passenger,self.figure,0,-currentSpeed)
                     #print("Current coordinate is ({0},{1})".format(self.passenger.xpos,self.passenger.ypos))
                 time.sleep(0.1)
         
         endtime = time.time()
-        print("[Behaviour Thread{0}] Passenger# {1} stopped at ({2},{3}), using time {4}".format(self.threadID,self.passenger.ID,self.passenger.xpos,self.passenger.ypos,endtime-starttime))
+        print("[Thread{}] Passenger# {} stopped at ({:.0f},{:.0f}), using time: {:.6f} ({:.6f}) seconds".format(self.threadID,self.passenger.ID,self.passenger.xpos,self.passenger.ypos,(endtime-starttime) * playspeed,endtime-starttime))
 
 class Generate(threading.Thread):
     def __init__(self, number):
@@ -146,13 +190,35 @@ class Generate(threading.Thread):
         #numbers of random passengers generated
         self.number = number
     def run(self):
+        threads = []
+        startTime = time.time()
         for i in range(self.number):
-            print("Generating passenger #{0}...".format(i))
-            exec("testPassenger{0} = genarate_passenger()".format(i))
-            exec("thread{0} = Behaviours({0},testPassenger{0})".format(i))
+            print("[Generation] Generating passenger #{0}...".format(i))
+            generate_passenger()
+        endTime = time.time()
+        global generationTime
+        generationTime = endTime - startTime
+        print("[Generation] Generation finished. {} passengers generated. Total time: {:.6f} seconds".format(self.number,generationTime))
+        runStartTime = time.time()
+        for i in range(self.number):
+            exec("thread{0} = Behaviours({0},passengers[{0}])".format(i))
+            exec("threads.append(thread{0})".format(i))
             exec("thread{0}.start()".format(i))
-            time.sleep(1)
-
+            time.sleep(interval / playspeed)
+        for t in threads:
+            t.join()
+        endTime = time.time()
+        global totalTime
+        global runTime
+        totalTime = endTime - startTime
+        runTime = endTime - runStartTime
+        print("[Main Thread] All passengers have stopped. The simulation terminated")
+        print('-'*30)
+        print("{:30s}{:10.6f} Seconds".format("Generation Time",generationTime))
+        print("{:30s}{:10.6f} Seconds".format("Simulation Time (Actual)",runTime))
+        print("{:30s}{:10.6f} Seconds".format("Total Runtime",totalTime))
+        print("\n{:30s}{:17.2f}x".format("Playspeed",playspeed))
+        print("{:30s}{:10.6f} Seconds".format("Simulation Time (Equvalent)",runTime * playspeed))
 #initialize the simulation
 def generate_simulation():
     global isPause
@@ -202,6 +268,17 @@ Label(frame1,text="Min Speed").pack(padx=10)
 entryMinSpeed = Entry(frame1,textvariable=var_min_speed)
 entryMinSpeed.pack(padx= 10,pady=10)
 
+var_interval = StringVar()
+var_interval.set(str(interval))
+Label(frame1,text="Interval").pack(padx=10)
+entryInterval = Entry(frame1,textvariable=var_interval)
+entryInterval.pack(padx= 10,pady=10)
+
+var_playSpeed = StringVar()
+var_playSpeed.set(str(playspeed))
+Label(frame1,text="Play Speed").pack(padx=10)
+entryPlaySpeed = Entry(frame1,textvariable=var_playSpeed)
+entryPlaySpeed.pack(padx= 10,pady=10)
 
 buttonSubmit = Button(frame1,text="OK",command=update_setting)
 buttonSubmit.pack(padx=10,pady=10)
@@ -213,12 +290,16 @@ buttonPause = Button(frame2, text="Pause", command=pause_simulation)
 buttonPause.pack(padx=5,pady=5)
 buttonPlay = Button(frame2, text="Play", command=play_simulation)
 buttonPlay.pack(padx=5,pady=5)
+buttonReplay = Button(frame2, text="Replay", command=play_simulation)
+buttonReplay.pack(padx=5,pady=5)
+buttonReset = Button(frame2, text="Reset", command=clear_simulation)
+buttonReset.pack(padx=5,pady=5)
 
 #canvas
 cv = Canvas(frame3,height = 400,width = 1100)
 
 #background image
-image_file = ImageTk.PhotoImage(Image.open(currentScene.backgroundPicture)) 
+image_file = ImageTk.PhotoImage(Image.open(currentScene.backgroundPicture))
 cv.create_image(0, 0, anchor='nw',image=image_file)
 
 #start point
